bc18d6d14a1c4e7442be321bd18f6d0429985b5a
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-01-19 17:39:19 +0100

    changed api endpoint HTTP methods to real ones

--- src/shopping/routes.rs
+++ src/shopping/routes.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 use crate::core::command::CommandHandler;
 use crate::shopping::application::order_command::{CreateOrderCommand, OrderSubmitPaymentCommand};
 
-#[get("/shopping/actions/create-order")]
+#[put("/shopping/actions/create-order")]
 pub async fn create_order() -> String {
     let create_order_result = CreateOrderCommand {
         customer_id: Uuid::new_v4(),
@@ -16,7 +16,7 @@ pub async fn create_order() -> String {
     format!("{}", create_order_result.unwrap())
 }
 
-#[get("/shopping/orders/<order_id>/actions/submit-payment")]
+#[put("/shopping/orders/<order_id>/actions/submit-payment")]
 pub async fn submit_order_payment(order_id: String) -> &'static str {
     let submit_order_payment_result = OrderSubmitPaymentCommand {
         order_id: Uuid::from_str(&order_id).unwrap(),
